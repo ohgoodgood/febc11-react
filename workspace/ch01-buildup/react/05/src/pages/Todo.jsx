514a1d3e2f18a27620f40ef0933846cd3ddf9fdb
ohgoodgood <jskim9504@gmail.com>
2024-11-12 16:14:33 +0900

    리액트 시작하기 진행 중

--- workspace/ch01-buildup/react/05/src/pages/Todo.jsx
+++ workspace/ch01-buildup/react/05/src/pages/Todo.jsx
@@ -1,15 +1,21 @@
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 
-function Todo(props){
+function Todo(props) {
   return (
     <div id="main">
       <div id="container">
         <ul>
           <li>
             <h2>쇼핑 목록</h2>
-            <TodoInput addItem={ props.addItem } />
-            <TodoList itemList={ props.itemList } toggleDone={ props.toggleDone } deleteItem={ props.deleteItem } />
+            <TodoInput addItem={props.addItem} />
+            <TodoList
+              itemList={props.itemList}
+              toggleDone={props.toggleDone}
+              deleteItem={props.deleteItem}
+            />
+            {/* <TodoList {...props} /> */}
+            {/* 무작정 다 넘겨버리는 건 좋은 생각은 아니다~ 하위 컴포넌트에서 쓰면 안되는 게 있을 수 있음 */}
           </li>
         </ul>
       </div>
