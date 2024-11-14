660aa045bfa3ceb6851aafe50a84cc49862a56d2
ohgoodgood <jskim9504@gmail.com>
2024-11-14 11:30:08 +0900

    자바스크립트 복습

--- workspace/ch01-buildup/js-review/08.js
+++ workspace/ch01-buildup/js-review/08.js
@@ -1,5 +1,5 @@
 // 지정한 수가 소수인지 여부를 반환
-var isPrime2 = function (num) {
+var isPrime = memo(function (num) {
   console.time("소요 시간");
   console.log("소수 판별 시작.", num);
 
@@ -16,23 +16,38 @@ var isPrime2 = function (num) {
   console.log("소수 판별 결과.", prime);
   console.timeEnd("소요 시간");
   return prime;
-};
+});
 
-var isPrime = function (num) {
-  // 캐시를 위한 코드
-  isPrime._cache = isPrime._cache || {};
-  if (isPrime._cache[num] !== undefined) {
-    console.log("cache hit!", num, isPrime._cache[num]);
-    // 캐시 되어 있는 경우 (cache hit)
-    return isPrime._cache[num];
-  } else {
-    // 캐시 안 되어 있는 경우
-    // 소수 판별 코드
-    return (isPrime._cache[num] = isPrime2(num));
-  }
-};
+// var isPrime = function (num) {
+//   // 캐시를 위한 코드
+//   isPrime._cache = isPrime._cache || {};
+//   if (isPrime._cache[num] !== undefined) {
+//     console.log("cache hit!", num, isPrime._cache[num]);
+//     // 캐시 되어 있는 경우 (cache hit)
+//     return isPrime._cache[num];
+//   } else {
+//     // 캐시 안 되어 있는 경우
+//     // 소수 판별 코드
+//     return (isPrime._cache[num] = isPrime2(num));
+//   }
+// };
 
 // cache 및 memoization은 순수함수(함수 내부로 전달되는 매개변수에 의해서만 값이 달라지는 함수)에 대해서만 의미가 있다. 전역변수 등의 함수 외부의 요소에 의해 결과값이 달라지는 함수는 순수함수가 아님!
+
+// 지정한 함수에 memoization 기능 추가 (범용성)
+function memo(fn) {
+  return function (args) {
+    fn._cache = fn._cache || {};
+    if (fn._cache[args] !== undefined) {
+      console.log("cache hit!", args, fn._cache[args]);
+      return fn._cache[args];
+    } else {
+      return (fn._cache[args] = fn(args));
+    }
+  };
+}
+
+// var isPrime = memo(isPrime); // memoization 기능 추가
 
 isPrime(1);
 isPrime(2);
