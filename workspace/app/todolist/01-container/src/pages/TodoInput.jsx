503a82d971e06f7305fa66175eb6ad03f0ddef79
ohgoodgood <jskim9504@gmail.com>
2024-11-19 16:34:06 +0900

    props에 전달되는 속성 명시

--- workspace/app/todolist/01-container/src/pages/TodoInput.jsx
+++ workspace/app/todolist/01-container/src/pages/TodoInput.jsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
-function TodoInput({ addItem }){
-  const [title, setTitle] = useState('');
+function TodoInput({ addItem }) {
+  const [title, setTitle] = useState("");
   const [nextId, setNextId] = useState(4);
 
   const handleAdd = () => {
-    if(title.trim() !== ''){
+    if (title.trim() !== "") {
       const item = { _id: nextId, title, done: false };
       addItem(item);
-      
+
       setNextId(nextId + 1);
-      setTitle('');
+      setTitle("");
     }
   };
-  
+
   const handleKeyUp = (event) => {
-    if(event.key === 'Enter') handleAdd();
+    if (event.key === "Enter") handleAdd();
   };
 
   return (
     <div className="todoinput">
-      <input type="text" autoFocus onKeyUp={ handleKeyUp } value={ title } onChange={ event => setTitle(event.target.value) } />
-      <button type="button" onClick={ handleAdd }>추가</button>
+      <input
+        type="text"
+        autoFocus
+        onKeyUp={handleKeyUp}
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        추가
+      </button>
     </div>
   );
 }
+
+TodoInput.propTypes = {
+  addItem: PropTypes.func.isRequired,
+};
 
 export default TodoInput;
