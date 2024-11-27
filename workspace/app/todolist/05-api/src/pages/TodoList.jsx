25a5d439e406262bc207486440e1b171ec15eb5c
ohgoodgood <jskim9504@gmail.com>
2024-11-27 11:46:00 +0900

    pagination enhancement

--- workspace/app/todolist/05-api/src/pages/TodoList.jsx
+++ workspace/app/todolist/05-api/src/pages/TodoList.jsx
@@ -4,6 +4,7 @@ import TodoListItem from "@pages/TodoListItem";
 import { useEffect, useRef, useState } from "react";
 import { Link, Outlet, useSearchParams } from "react-router-dom";
 import "../Pagination.css";
+import Pagination from "@components/Pagination";
 
 // const dummyData = {
 //   items: [{
@@ -25,7 +26,7 @@ function TodoList() {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const params = {
-    keyword: searchParams.get("keyword"),
+    keyword: searchParams.get("keyword") || "",
     page: searchParams.get("page") || 1,
     limit: 5,
   };
@@ -76,24 +77,6 @@ function TodoList() {
     setSearchParams(new URLSearchParams(`keyword=${searchRef.current.value}`));
   };
 
-  let pageList = [];
-  const current = data?.pagination.page;
-
-  for (let page = 1; page <= data?.pagination.totalPages; page++) {
-    searchParams.set("page", page);
-    // keyword=환승&page=1
-    // keyword=환승&page=2
-    // keyword=환승&page=3
-
-    let search = searchParams.toString();
-
-    pageList.push(
-      <li className={current === page ? "active" : ""}>
-        <Link to={`/list?${search}`}>{page}</Link>
-      </li>
-    );
-  }
-
   return (
     <div id="main">
       <h2>할일 목록</h2>
@@ -112,11 +95,12 @@ function TodoList() {
         <ul className="todolist">{itemList}</ul>
       </div>
 
-      <div className="pagination">
-        <ul>{pageList}</ul>
-      </div>
-
-      <Outlet />
+      {data && (
+        <Pagination
+          totalPages={data?.pagination.totalPages}
+          current={data?.pagination.page}
+        />
+      )}
     </div>
   );
 }
