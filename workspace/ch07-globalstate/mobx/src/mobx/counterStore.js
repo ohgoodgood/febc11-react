f12303ee34bf4ceba41263d172a7d294424992dc
ohgoodgood <jskim9504@gmail.com>
2024-12-02 09:47:07 +0900

    mobx

---
+++ workspace/ch07-globalstate/mobx/src/mobx/counterStore.js
@@ -0,0 +1,23 @@
+import { makeAutoObservable } from "mobx";
+
+class CounterStore {
+  count = 0;
+
+  constructor() {
+    makeAutoObservable(this); // 상태 관찰
+  }
+
+  countUp(step) {
+    this.count += step;
+  }
+
+  countDown(step) {
+    this.count -= step;
+  }
+
+  countReset() {
+    this.count = 0;
+  }
+}
+
+export default new CounterStore();
