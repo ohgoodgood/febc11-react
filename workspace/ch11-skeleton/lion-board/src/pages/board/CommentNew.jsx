fe63cf83c2f44cca19612e1c98c71b9fe9e4055d
ohgoodgood <jskim9504@gmail.com>
2024-12-10 01:12:12 +0900

    댓글 등록 기능

--- workspace/ch11-skeleton/lion-board/src/pages/board/CommentNew.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/CommentNew.jsx
@@ -1,18 +1,59 @@
+import InputError from "@components/InputError";
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+
 export default function CommentNew() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const axios = useAxiosInstance();
+
+  const { _id } = useParams();
+
+  const queryClient = useQueryClient();
+
+  const addReply = useMutation({
+    mutationFn: (formData) => {
+      const body = {
+        content: formData.comment,
+        name: formData.name,
+      };
+      return axios.post(`/posts/${_id}/replies`, body);
+    },
+
+    onSuccess: () => {
+      alert("댓글이 등록되었습니다.");
+      queryClient.invalidateQueries(["replies", _id]);
+      // navigate(`/${type}/${_id}`);
+      reset();
+    },
+
+    onError: (err) => {
+      console.error(err);
+    },
+  });
+
   return (
     <div className="p-4 border border-gray-200 rounded-lg">
       <h4 className="mb-4">새로운 댓글을 추가하세요.</h4>
-      <form action="#">
+      <form onSubmit={handleSubmit(addReply.mutate)}>
         <div className="mb-4">
           <textarea
             rows="3"
             cols="40"
             className="block p-2 w-full text-sm border rounded-lg border-gray-300 bg-gray-50 focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             placeholder="내용을 입력하세요."
-            name="comment"
+            // name="comment"
+            {...register("comment", { required: "내용을 입력해주세요." })}
           ></textarea>
 
-          <p className="ml-2 mt-1 text-sm text-red-500">내용은 필수입니다.</p>
+          <InputError target={errors.comment} />
         </div>
         <button
           type="submit"
