473732915f007b87f75b3c335d81dcd926000cfa
ohgoodgood <jskim9504@gmail.com>
2024-12-10 14:15:29 +0900

    실명 계정 토큰으로 변경(하드코딩)

--- workspace/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
+++ workspace/ch11-skeleton/lion-board/src/hooks/useAxiosInstance.js
@@ -16,7 +16,7 @@ function useAxiosInstance() {
     // 요청이 전달되기 전에 필요한 공통 작업 수행
     config.headers[
       "Authorization"
-    ] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjQwLCJ0eXBlIjoidXNlciIsIm5hbWUiOiJob2hvIiwiZW1haWwiOiJob2hvQGhvaG8uY29tIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzM3MjU1NjIsImV4cCI6MTczMzgxMTk2MiwiaXNzIjoiRkVTUCJ9.hfV7r8rsbuiOdi6AhzKBpPFtfjBz-FTDKUfGqXw9MA0`;
+    ] = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjc5LCJ0eXBlIjoidXNlciIsIm5hbWUiOiLquYDso7zshJ0iLCJlbWFpbCI6ImFhQGFhLmNvbSIsImltYWdlIjp7Im9yaWdpbmFsbmFtZSI6IklNR18yMjU4LkpQRyIsIm5hbWUiOiJpWUNLWHNwTGIuSlBHIiwicGF0aCI6Ii9maWxlcy8wMC1icnVuY2gvaVlDS1hzcExiLkpQRyJ9LCJsb2dpblR5cGUiOiJlbWFpbCIsImlhdCI6MTczMzgwNTg5MCwiZXhwIjoxNzMzODkyMjkwLCJpc3MiOiJGRVNQIn0.PAuDqNfS3jSoOh2ZxLSSDYKqLUlclZ79e9uOtlEm1js`;
     config.params = {
       delay: 500,
       ...config.params, // 기존 쿼리스트링 복사
