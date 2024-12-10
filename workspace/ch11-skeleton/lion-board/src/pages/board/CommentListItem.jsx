0fe35be06400b04d7d2514cdff93cd0dd2e61f8f
ohgoodgood <jskim9504@gmail.com>
2024-12-10 13:04:46 +0900

    댓글 삭제 기능

--- workspace/ch11-skeleton/lion-board/src/pages/board/CommentListItem.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/CommentListItem.jsx
@@ -1,5 +1,7 @@
+import useAxiosInstance from "@hooks/useAxiosInstance";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 CommentListItem.propTypes = {
   item: PropTypes.shape({
@@ -17,6 +19,21 @@ CommentListItem.propTypes = {
 };
 
 export default function CommentListItem({ item }) {
+  const queryClient = useQueryClient();
+
+  const axios = useAxiosInstance();
+
+  const { _id } = useParams();
+
+  const removeItem = useMutation({
+    mutationFn: (_id) => axios.delete(`/posts/${_id}/replies/${item._id}`),
+
+    onSuccess: () => {
+      alert("댓글이 삭제되었습니다.");
+      queryClient.invalidateQueries({ queryKey: ["replies", _id] });
+    },
+  });
+
   return (
     <div className="shadow-md rounded-lg p-4 mb-4">
       <div className="flex justify-between items-center mb-2">
@@ -35,15 +52,14 @@ export default function CommentListItem({ item }) {
         </time>
       </div>
       <div className="flex justify-between items-center mb-2">
-        <form action="#">
-          <pre className="whitespace-pre-wrap text-sm">{item.content}</pre>
-          <button
-            type="submit"
-            className="bg-red-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-          >
-            삭제
-          </button>
-        </form>
+        <pre className="whitespace-pre-wrap text-sm">{item.content}</pre>
+        <button
+          type="submit"
+          onClick={() => removeItem.mutate(_id)}
+          className="bg-red-500 py-1 px-2 text-sm text-white font-semibold ml-2 hover:bg-amber-400 rounded"
+        >
+          삭제
+        </button>
       </div>
     </div>
   );
