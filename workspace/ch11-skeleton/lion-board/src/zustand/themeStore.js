71f4548b82c7f7a7d47fbd751c797ecbe5455048
ohgoodgood <jskim9504@gmail.com>
2024-12-11 11:50:17 +0900

    m.adj

--- workspace/ch11-skeleton/lion-board/src/zustand/themeStore.js
+++ workspace/ch11-skeleton/lion-board/src/zustand/themeStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 // 상태 정보를 메모리(새로고침하면 다 날아감)가 아닌 스토리지에 저장해주는 기능 - zustand에서 제공됨
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 const useThemeStore = create(
   persist(
