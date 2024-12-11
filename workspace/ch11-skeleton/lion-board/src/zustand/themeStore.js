c4a25418950b682f3dcf989237ca19683142eead
ohgoodgood <jskim9504@gmail.com>
2024-12-11 17:48:36 +0900

    주석 추가

--- workspace/ch11-skeleton/lion-board/src/zustand/themeStore.js
+++ workspace/ch11-skeleton/lion-board/src/zustand/themeStore.js
@@ -6,14 +6,18 @@ const useThemeStore = create(
   persist(
     // 기존 store 함수
     (set) => ({
-      // isDarkMode: false, // 상태 관리 대상. 초기값: 라이트모드
       isDarkMode: window.matchMedia("(prefers-color-scheme: dark)").matches
         ? true
-        : false, // 상태 관리 대상. 초기값: 시스템 세팅에 따라
-      toggleTheme: () => set((state) => ({ isDarkMode: !state.isDarkMode })), // 상태 수정
+        : false,
+      toggleTheme: () => set((state) => ({ isDarkMode: !state.isDarkMode })),
+      // set 함수를 호출하는 두 가지 방식
+      // 객체를 직접 전달: set({key: value})
+      // 업데이트 함수를 전달: set((state) => ({key: value}))
+      // 두 번째 방식에서, zustand가 현재 스토어에 저장된 상태(state)를 업데이트 함수의 인자로 전달함.
+      // 따라서, 위 코드에서 state는 스토어에 저장된 모든 상태(isDarkMode 포함)의 최신 값을 포함함.
+      // 업데이트 함수가 새로운 상태를 반환하면, zustand는 스토어를 업데이트하고 변경 사항을 구독 중인 컴포넌트에 알림.
     }),
 
-    // 스토리지 저장 옵션
     {
       name: "themeStore",
       // createJSONStorage()의 기본값은 localStorage
