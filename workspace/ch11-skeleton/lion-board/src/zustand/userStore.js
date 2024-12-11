1f8b38e36915e138e4b518b1ad547f49954719a4
ohgoodgood <jskim9504@gmail.com>
2024-12-11 17:34:10 +0900

    주석 추가

--- workspace/ch11-skeleton/lion-board/src/zustand/userStore.js
+++ workspace/ch11-skeleton/lion-board/src/zustand/userStore.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
-// 상태 정보를 메모리(새로고침하면 다 날아감)가 아닌 스토리지에 저장해주는 기능 - zustand에서 제공됨
+// `persist` 미들웨어: 상태 정보를 메모리(새로고침하면 다 날아감)가 아닌 스토리지에 저장해주는 기능 - zustand에서 제공됨
 import { persist, createJSONStorage } from "zustand/middleware";
 
+// store 생성
 // zustand 내부의 create 함수가 실행됨 -> UserStore가 호출됨 -> set, get, subscribe 함수 등을 생성하여 UserStore에 인자로 전달함 -> UserStore 내부에서 해당 함수들 사용하여 상태 업데이트 가능
 const UserStore = (set) => ({
   user: null,
