0d566e95f6040cccba2e44db329725030f5a639f
ohgoodgood <jskim9504@gmail.com>
2024-12-12 11:27:10 +0900

    helmet 기능 사용

--- workspace/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/Detail.jsx
@@ -2,6 +2,7 @@ import useAxiosInstance from "@hooks/useAxiosInstance";
 import CommentList from "@pages/board/\bCommentList";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import useUserStore from "@zustand/userStore";
+import { Helmet } from "react-helmet-async";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 export default function Detail() {
@@ -49,47 +50,55 @@ export default function Detail() {
   }
 
   return (
-    <main className="container mx-auto mt-4 px-4">
-      <section className="mb-8 p-4">
-        <div className="font-semibold text-xl">{data.item.title}</div>
-        <div className="text-right text-gray-400">{data.item.user.name}</div>
-        <div className="mb-4">
-          <div>
-            <pre className="font-roboto w-full p-2 whitespace-pre-wrap">
-              {data.item.content}
-            </pre>
+    <>
+      <Helmet>
+        <title>{data.item.title} - 멋사컴</title>
+        <meta property="og:title" content={data.item.title} />
+        <meta property="og:description" content={data.item.content} />
+      </Helmet>
+
+      <main className="container mx-auto mt-4 px-4">
+        <section className="mb-8 p-4">
+          <div className="font-semibold text-xl">{data.item.title}</div>
+          <div className="text-right text-gray-400">{data.item.user.name}</div>
+          <div className="mb-4">
+            <div>
+              <pre className="font-roboto w-full p-2 whitespace-pre-wrap">
+                {data.item.content}
+              </pre>
+            </div>
+            <hr />
           </div>
-          <hr />
-        </div>
-        <div className="flex justify-end my-4">
-          <Link
-            to={`/${type}`}
-            className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-          >
-            목록
-          </Link>
+          <div className="flex justify-end my-4">
+            <Link
+              to={`/${type}`}
+              className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
+            >
+              목록
+            </Link>
 
-          {user?._id === data.item.user._id && (
-            <>
-              <Link
-                to={`/${type}/${_id}/edit`}
-                className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-              >
-                수정
-              </Link>
-              <button
-                type="submit"
-                onClick={onSubmit}
-                className="bg-red-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-              >
-                삭제
-              </button>
-            </>
-          )}
-        </div>
-      </section>
+            {user?._id === data.item.user._id && (
+              <>
+                <Link
+                  to={`/${type}/${_id}/edit`}
+                  className="bg-gray-900 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
+                >
+                  수정
+                </Link>
+                <button
+                  type="submit"
+                  onClick={onSubmit}
+                  className="bg-red-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
+                >
+                  삭제
+                </button>
+              </>
+            )}
+          </div>
+        </section>
 
-      <CommentList />
-    </main>
+        <CommentList />
+      </main>
+    </>
   );
 }
