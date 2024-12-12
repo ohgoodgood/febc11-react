dcf250657d0b606d0ce3a83e7d0f1fc55756d2d0
ohgoodgood <jskim9504@gmail.com>
2024-12-12 09:31:43 +0900

    조건부 렌더링 추가

--- workspace/ch11-skeleton/lion-board/src/pages/board/List.jsx
+++ workspace/ch11-skeleton/lion-board/src/pages/board/List.jsx
@@ -1,10 +1,13 @@
 import useAxiosInstance from "@hooks/useAxiosInstance";
 import ListItem from "@pages/board/ListItem";
 import { useQuery } from "@tanstack/react-query";
+import useUserStore from "@zustand/userStore";
 import { Link, useParams } from "react-router-dom";
 
 export default function List() {
   const axios = useAxiosInstance();
+
+  const { user } = useUserStore();
 
   const { type } = useParams();
 
@@ -45,12 +48,14 @@ export default function List() {
           </button>
         </form>
 
-        <Link
-          to="new"
-          className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
-        >
-          글작성
-        </Link>
+        {user && (
+          <Link
+            to="new"
+            className="bg-orange-500 py-1 px-4 text-base text-white font-semibold ml-2 hover:bg-amber-400 rounded"
+          >
+            글작성
+          </Link>
+        )}
       </div>
       <section className="pt-10">
         <table className="border-collapse w-full table-fixed">
